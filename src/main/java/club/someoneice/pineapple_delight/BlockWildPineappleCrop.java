a947e1304ed16bfedf864e5f91722d4804c5ce35
AmarokIce <snowlyicewolf@outlook.com>
2025-11-16 18:25:05 +0800

    🐺🧹

--- src/main/java/club/someoneice/pineapple_delight/BlockWildPineappleCrop.java
+++ src/main/java/club/someoneice/pineapple_delight/BlockWildPineappleCrop.java
@@ -1,9 +1,9 @@
 package club.someoneice.pineapple_delight;
 
-import net.minecraft.world.level.block.BaseCoralPlantBlock;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BushBlock;
 
-public class BlockWildPineappleCrop extends BaseCoralPlantBlock {
+public class BlockWildPineappleCrop extends BushBlock {
   public BlockWildPineappleCrop() {
     super(Properties.copy(Blocks.WHEAT));
   }
