dc8e744e45c0cffcc3a302e7ff390ce8bd581821
AmarokIce <snowlyicewolf@outlook.com>
2026-08-12 23:04:50 +0800

    🐺✨📥

--- src/main/java/club/someoneice/pineapple/data/BlockUtil.java
+++ src/main/java/club/someoneice/pineapple/data/BlockUtil.java
@@ -2,13 +2,22 @@ package club.someoneice.pineapple.data;
 
 import club.someoneice.pineapple.common.BlockPineappleCrop;
 import club.someoneice.pineapple.init.ItemList;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.storage.loot.LootParams;
 import vectorwing.farmersdelight.common.block.PieBlock;
 import vectorwing.farmersdelight.common.block.WildCropBlock;
+
+import java.util.List;
 
 public class BlockUtil {
   public static Block pie() {
@@ -20,7 +29,14 @@ public class BlockUtil {
   }
 
   public static Block pineappleCrate() {
-    return new Block(Properties.ofFullCopy(Blocks.OAK_PLANKS));
+    return new Block(Properties.ofFullCopy(Blocks.OAK_PLANKS)) {
+      @Override
+      protected List<ItemStack> getDrops(BlockState state, LootParams.Builder params) {
+        final var list = super.getDrops(state, params);
+        list.add(this.asItem().getDefaultInstance());
+        return list;
+      }
+    };
   }
 
   public static Block wildBlock() {
